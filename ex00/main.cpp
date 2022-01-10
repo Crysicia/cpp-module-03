59beaef25c1740d8f11ca84d3575c084b242c792
Crysicia <7459685+Crysicia@users.noreply.github.com>
2022-01-10 17:19:38 +0100

    Add tests in ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,12 +1,26 @@
 #include "ClapTrap.hpp"
 
 int main(void) {
+	std::cout << "--- Initialize ClapTraps ----" << std::endl;
 	ClapTrap atlas("Atlas");
 	ClapTrap notAtlas(atlas);
 	ClapTrap notAtlasAgain = atlas;
 
+	std::cout << std::endl << "--- Call attack() on ClapTraps ----" << std::endl;
 	atlas.attack("Jimmy");
 	notAtlas.attack("Jimmy");
 	notAtlasAgain.attack("Jimmy");
+
+	std::cout << std::endl << "--- Call takeDamage() on ClapTraps ----" << std::endl;
+	atlas.takeDamage(42);
+	notAtlas.takeDamage(42);
+	notAtlasAgain.takeDamage(42);
+
+	std::cout << std::endl << "--- Call beRepaired() on ClapTraps ----" << std::endl;
+	atlas.beRepaired(42);
+	notAtlas.beRepaired(42);
+	notAtlasAgain.beRepaired(42);
+
+	std::cout << std::endl << "--- Destroy ClapTraps ----" << std::endl;
 	return 0;
 }
